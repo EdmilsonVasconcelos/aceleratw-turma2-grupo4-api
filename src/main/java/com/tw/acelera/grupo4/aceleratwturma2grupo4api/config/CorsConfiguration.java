129c9f30f1f979835ee6687a7a6cd9e44e85363c
Edmilson de França Vasconcelos Junior <edmilsonvasconcelosjunior@MacBook-Pro-de-Edmilson.local>
2021-06-05 20:39:05 -0300

    ajustes nos cors

--- src/main/java/com/tw/acelera/grupo4/aceleratwturma2grupo4api/config/CorsConfiguration.java
+++ src/main/java/com/tw/acelera/grupo4/aceleratwturma2grupo4api/config/CorsConfiguration.java
@@ -2,15 +2,15 @@ package com.tw.acelera.grupo4.aceleratwturma2grupo4api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("https://good-carango-api.herokuapp.com")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    	registry.addMapping("/**");
     }
 }
