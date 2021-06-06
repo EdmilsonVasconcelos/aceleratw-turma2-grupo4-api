f45a4d624d02b2a8ed9273995c866c79c9f69064
Edmilson de França Vasconcelos Junior <edmilsonvasconcelosjunior@MacBook-Pro-de-Edmilson.local>
2021-06-05 21:27:31 -0300

    ajustes no cors

--- src/main/java/com/tw/acelera/grupo4/aceleratwturma2grupo4api/config/CorsConfiguration.java
+++ src/main/java/com/tw/acelera/grupo4/aceleratwturma2grupo4api/config/CorsConfiguration.java
@@ -11,6 +11,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-    	registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedOrigins("https://good-carango-api.herokuapp.com")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
